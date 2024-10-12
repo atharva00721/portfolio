24defe8ba86b9435b194d6d4507187647544a19b
atharva00721 <69038796+atharva00721@users.noreply.github.com>
2024-10-13 02:56:59 +0530

    removed Skeleton

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -67,9 +67,7 @@ const TechStack = () => (
     </p>
   </div>
 );
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
+
 const ImgMe = () => (
   <Image
     src="/images/me.jpg"
